e2745c0394b9e2c472a56f379f55f0c70224437e
nourelkawafi <74060642+nourelkawafi@users.noreply.github.com>
2021-02-06 04:42:41 +0200

    coco2

--- src/main/java/cocoMelon.java
+++ src/main/java/cocoMelon.java
@@ -2,5 +2,6 @@ public class cocoMelon {
     public static void main(String[] args) {
         System.out.println("coco");
         System.out.println("Melon");
+        System.out.println("Coco Melon");
     }
 }
