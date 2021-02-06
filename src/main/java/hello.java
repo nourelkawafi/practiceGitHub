70687b805c6409fe637ebfb5aff5f0eca82d7e0d
nourelkawafi <74060642+nourelkawafi@users.noreply.github.com>
2021-02-06 04:28:12 +0200

    hh

--- src/main/java/hello.java
+++ src/main/java/hello.java
@@ -1,2 +1,6 @@
-package PACKAGE_NAME;public class hello {
+public class hello {
+    //jhik
+    // how are you
+
+
 }
--- src/test/java/nour.java
+++ src/test/java/nour.java
@@ -1,2 +1,5 @@
-package PACKAGE_NAME;public class nour {
+public class nour {
+    public static void main(String[] args) {
+
+    }
 }
